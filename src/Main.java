6572d7cb2341a702aff397119e0719d5abbea5b2
ZorinFilkov <zfilkov@abv.bg>
2016-03-02 21:22:47 +0200

    HwT08-

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void f1(String name) {
+		System.out.println("Greetings, " + name + "!");
+
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		f1("Ivan");
+
+	}
+
+}
